32ea68ea6e4b4d742c23df71da02b5cb10a2ffb0
fegnying <215132162@qq.com>
2018-03-25 22:33:25 +0800

    tabContent

---
+++ src/containers/window/task/content.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Feng Ying on 2018/3/25.
+ */
+import React from 'react'
+import TabContent from './tabContent'
+
+class Content extends React.Component{
+
+    render(){
+        return(
+           <TabContent />
+
+        )
+    }
+}
+export default Content
--- src/containers/window/task/toTask.js
+++ src/containers/window/task/toTask.js
@@ -4,12 +4,14 @@
 import React from 'react'
 import BackTitle from '../public/backTitle'
 import {Row} from 'antd'
+import Content from './content'
 
 class ToTask extends React.Component{
     render() {
         return (
             <Row>
                 <BackTitle title="发布任务"/>
+                <Content />
             </Row>
 
         )
